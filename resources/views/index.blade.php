3f32d1ecb15db96c0b0e8972600b85c1c4b1762e
Golden Moses <macbookair2020m1@Goldens-MacBook-Air.local>
2022-05-23 01:09:37 +0100

    finished implementation of sidebar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Sidebars · Bootstrap v5.0</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css" rel="stylesheet">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sidebars/">
+
+    <script src="/js/jquery-3.6.0.min.js"></script>
+
+  </head>
+  <body>
+
+
+<div class="d-flex">
+
+    <aside>
+        <div class="d-flex flex-column flex-shrink-0 bg-primary" style="width: 4.5rem;">
+
+            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center mt-5">
+
+            <li>
+                <a href="#" class="nav-link py-3 text-white" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <i data-feather="search"></i>
+                </a>
+            </li>
+
+              <li>
+                <a href="#" class="nav-link py-3 text-white" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <i data-feather="user"></i>
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="nav-link py-3 text-white" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <i data-feather="users"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="nav-link py-3 text-white" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <i data-feather="message-circle"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="nav-link py-3 text-white" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <i data-feather="file"></i>
+                </a>
+              </li>
+
+
+
+            </ul>
+            <div class="dropdown mb-5">
+
+                <a href="#" class="nav-link py-3 text-white" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <i data-feather="settings"></i>
+                </a>
+              <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" class="rounded-circle border">
+              </a>
+              <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
+                <li><a class="dropdown-item" href="#">New project...</a></li>
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Sign out</a></li>
+              </ul>
+              <a role="button" class="nav-link py-3 text-white" title="Dashboard" data-bs-toggle="collapse" data-bs-target="#sidebarmenu">
+                  <i data-feather="align-left"></i>
+              </a>
+
+            </div>
+        </div>
+
+
+
+            <div class="collapse show" id="sidebarmenu">
+
+                <div class="d-flex flex-column flex-shrink-0 bg-light pt-5" style="width: 280px;">
+
+                    <ul class="nav nav-pills flex-column mb-auto">
+
+                        <a href="#" class="nav-link active link-dark">
+                            <i data-feather="layout"></i>
+                        Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="users"></i>
+                        Users
+                        </a>
+                    </li>
+                    <li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="folder"></i>
+                        Departments
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="users"></i>
+                        Employee
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="zap"></i>
+                        Activities
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="check-circle"></i>
+                        Holidays
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="play-circle"></i>
+                        Events
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="credit-card"></i>
+                        Payroll
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="user"></i>
+                        Accounts
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link link-dark">
+                            <i data-feather="alert-circle"></i>
+                        Report
+                        </a>
+                    </li>
+
+                    </ul>
+                </div>
+
+        </div>
+
+
+
+    </aside>
+
+
+    <div class="content d-flex flex-column">
+
+        <nav class="d-flex p-3">
+            <h3>Users</h3>
+
+            <div class="form-group">
+              <input type="number" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+            </div>
+
+            <div class="form-group">
+              <input type="search" name="" id="" class="form-control" placeholder="Search..." aria-describedby="helpId">
+              <i data-feather="search"></i>
+            </div>
+
+        </nav>
+        <div class="">
+
+
+
+        </div>
+    </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/index.js"></script>
+
+    <script src="/js/feather.min.js"></script>
+
+    <script>
+        feather.replace()
+
+    </script>
+
+  </body>
+</html>
