9b8d4bbb02871e3e40da0b3e49ee23cb96ef81a0
Golden Moses <macbookair2020m1@Goldens-MacBook-Air.local>
2022-05-23 16:04:33 +0100

    finished table design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -261,7 +261,7 @@
         <div class="container-fluid">
             <div class="d-flex justify-content-end mb-4">
 
-            <button class="btn btn-success rad-7">Add User</button>
+            <button class="btn rad-7" style="background: #227056; color:#fff; font-size: 12px">Add User</button>
             </div>
 
             <div class="bg-light border border-primary">
@@ -348,6 +348,8 @@
             </div>
 
         </div>
+
+        <footer><span class="text-grey">&copy; copyright 2022</span> BingHr.io</footer>
     </div>
 
 
@@ -374,9 +376,18 @@
             $('#data-table').DataTable({
                 "paging":   false,
                 "ordering": false,
-                "info":     false
-            });
+                "info":     false,
+
+                language: {
+                    search: "_INPUT_",
+                    searchPlaceholder: "Search..."
+                    }
+                });
+
+                $("#data-table_filter label").append('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search" style="position: relative;right: 58px;width: 18px;"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>');
+                $("#data-table_filter").prepend('<h6 class="float-left ml-2 mt-4">List Users</h6>');
         } );
+
 
     </script>
 
